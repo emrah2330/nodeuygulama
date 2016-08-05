4553f1927c36af58fbd2a01503b205a442b5ab0e
emrahkilic <emrahkilic2330@gmail.com>
2016-08-05 18:07:30 +0300

    İkinci Commit

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-var http = require('http');
-var server = http.createServer(function(req,res){
-	res.writeHead(200,{'Content-Type':'text/html'});
-	res.end('<h1>Merhaba Dünya</h1>');
-});
+var express = require('express');
+var app = express();
 var port = Number(process.env.PORT || 8080);
-server.listen(port);
+var server = app.listen(port);
+var io = require('socket.io').listen(server);
+
+app.get('/',function(req,res){
+	res.sendFile(__dirname+'/index.html');
+});
+
+io.sockets.on('connection',function(socket){ //Kullanıcı eğer bu sayfaya bağlanırsa çalışacak ana fonksiyonum
+	socket.on('yorumgonder',function(data){ // socket.emit('yorumgonder',yorum) ile gönderdiğim değeri burada aldım.
+		io.emit('yorumugerigonder',data); //Bu fonksiyon ise gelen yorumu alıp bütün kullanıcılara servis eder.Bu değeri js dosyamda socket on ile alıp ekrana yazdırabilirim.
+	});
+});
